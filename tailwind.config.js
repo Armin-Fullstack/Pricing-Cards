ba960522dfbc2580af919f288cf14c8051531681
Armin Hossein Zadeh <arminfrontend@gmail.com>
2023-01-14 20:08:43 +0330

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./**/*.{html,js}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ["Merriweather Sans" , "sans-serif"]
+      }
+    },
+  },
+  plugins: [],
+}
